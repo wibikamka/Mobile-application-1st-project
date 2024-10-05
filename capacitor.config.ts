5b58fc885c7915e28b9dca26a113d4bfde6e9c12
wibikamka <nurwibisanakamka@gmail.com>
2024-10-05 09:34:17 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'moba-sesi-5',
+  webDir: 'www'
+};
+
+export default config;
