ef82d80df708e56d65bb4af97f09aeb81ba56b79
wibikamka <nurwibisanakamka@gmail.com>
2024-10-08 14:20:21 +0700

    first commit

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -9,9 +9,23 @@ import { ActivatedRoute } from '@angular/router';
 export class FolderPage implements OnInit {
   public folder!: string;
   private activatedRoute = inject(ActivatedRoute);
+  judul: string = ""
+  judul_fix: string = "Judul Default"
+  subtitle: string = ""
+  subtitle_fix: string = "Subtitle Default"
+  deskripsi: string = ""
+  deskripsi_fix: string = "Deskripsi Default"
   constructor() {}
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
   }
+
+  ubahjudul(){
+    this.judul_fix=this.judul
+    this.deskripsi_fix=this.deskripsi
+    this.subtitle_fix=this.subtitle
+
+  }
+ 
 }
